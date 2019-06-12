fafc9723a0c4b996bd36d1dd6dffaac83382f166
刘跃灿 <995611928@qq.com>
2019-06-12 16:00:39 +0800

    Init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using JsonUseInNetCoreProject.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+
+namespace JsonUseInNetCoreProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Dictionary<string, string> _infodictionary = new Dictionary<string, string>();
+            _infodictionary.Add("username","gamecc666");
+            _infodictionary.Add("age", "16");
+            _infodictionary.Add("gender", "男");
+            _infodictionary.Add("heigh", "183");
+            _infodictionary.Add("weight", "78");
+
+            string _jsonstr = JsonConvert.SerializeObject(_infodictionary);
+            PersionData _sb = JsonConvert.DeserializeObject<PersionData>(_jsonstr);
+            return Content("转换后的Json字符串："+_jsonstr+" ||解析后的类中的字段值 用户名字："+_sb.username);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/PersionData.cs
@@ -0,0 +1,11 @@
+﻿namespace JsonUseInNetCoreProject.Models
+{
+    public class PersionData
+    {
+        public string username { get; set; }
+        public string age { get; set; }
+        public string gender { get; set; }
+        public string heigh { get; set; }
+        public string weight { get; set; }
+    }
+}
